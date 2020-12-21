cf933244b664a65309c49686fbcf5f8591161a2f
rookiemonkey <kevinroirigorbasina@pm.me>
2020-12-22 07:41:59 +0800

    add adjectives array instead of faker adjectives

---
+++ src/models/_adjective.js
@@ -0,0 +1,16 @@
+module.exports = [
+    'Ergonomic', 'Small', 'Handmade', 'Handcrafted', 'Gorgeous',
+    'Practical', 'Rustic', 'Licensed', 'Unbranded', 'Sleek', 'Incredible',
+    'Awesome', 'Tasty', 'Refined', 'Intelligent', 'Fantastic', 'Generic',
+    'Fine', 'Staple', 'Guarantee', 'Unconditional', 'Promise', 'Risk-free',
+    'Pledge', 'Best-seller', 'Easy', 'Simple', 'Smooth', 'Painless', 'Safe',
+    'Tested', 'Fresh', 'Pristine', 'Brand-new', 'Premium', 'Luxurious', 'Secret',
+    'Limited', 'Rare', 'Few', 'Limited Edition', 'Unique', 'Select', 'Authentic',
+    'Legitimate', 'Revolutionary', 'Extraordinary', 'Amazing', 'Remarkable', 'Startling',
+    'Sensational', 'Magic', 'Miracle', 'Dazzling', 'Ravishing', 'Brilliant', 'Honeyed',
+    'Compelling', 'Ultra', 'Plethora', 'Zesty', 'Staggering', 'Topical', 'Trustworthy',
+    'Free', 'Powerful', 'Safe and effective', 'Complimentary', 'Convenient', 'Miracle',
+    'Great', 'Exciting', 'Adorable', 'Beautiful', 'Clean', 'Drab', 'Elegant', 'Fancy',
+    'Glamorous', 'Magnificent', 'Old-fashioned', 'Plain', 'Quaint', 'Sparkling', 'Ancient',
+    'Modern'
+]

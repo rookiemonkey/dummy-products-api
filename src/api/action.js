8e96d25fab4d18f589ec70ffc6febfd4f818d484
rookiemonkey <kevinroirigorbasina@pm.me>
2020-12-23 09:59:05 +0800

    update docs

--- src/api/action.js
+++ src/api/action.js
@@ -9,28 +9,28 @@ const Product = require('../models/Product');
 const checkout = handleAsync(async (req, res, next) => {
     const { items } = req.body;
     const date = new Date();
-    const bought = new Array();
-    let total = 0;
+    const itemsBought = new Array();
+    let totalAmount = 0;
 
     for (let i = 0; i < items.length; i++) {
         const foundItem = await Product
             .findById(items[i])
             .select(`-product_reviews -product_description 
-            -product_image_sm -product_image_md -product_image_lg
             -product_stock -product_color -product_material -product_ratings
             -product_sales -product_department -product_departmentId -product_type
             -__v`)
 
-        total += foundItem.product_price;
-        bought.push(foundItem);
+        totalAmount += foundItem.product_price;
+        itemsBought.push(foundItem);
     }
 
     res.json({
         success: true,
         datatype: 'CHECKOUT',
         date: `${date.toDateString()} ${date.toLocaleTimeString()}`,
-        total,
-        bought
+        totalItems: itemsBought.length,
+        totalAmount,
+        itemsBought,
     })
 })
 

ccbbc5f11ab9afe91a2fff7c1b80ebaf47f283c9
rookiemonkey <kevinroirigorbasina@pm.me>
2021-01-04 13:14:53 +0800

    update review model

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,18 +1,21 @@
-const faker = require('faker');
 const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema({
     review_name: {
         type: String,
-        default: () => faker.name.findName()
+        required: [true, `Reviewer's name is required`]
     },
     review_details: {
         type: String,
-        default: () => faker.lorem.sentence()
+        required: [true, `Review details is required`]
     },
     review_rating: {
         type: Number,
         default: () => Math.floor(Math.random() * 6)
+    },
+    review_avatar: {
+        type: String,
+        required: [true, `Reviewer's avatar is required`]
     },
     review_productid: {
         type: mongoose.Schema.Types.ObjectId,
